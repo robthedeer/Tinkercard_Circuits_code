deaf8ae737ba2a47eaff44cb6064c8d918fb5fce
Rob_The_Deer <86634577+robthedeer@users.noreply.github.com>
2024-11-17 15:19:36 +0300

    Update cake.cpp

--- cake.cpp
+++ cake.cpp
@@ -1,95 +1,40 @@
-const cards = document.querySelectorAll(".card"),
-timeTag = document.querySelector(".time b"),
-flipsTag = document.querySelector(".flips b"),
-refreshBtn = document.querySelector(".details button");
+int dummy = 0;
 
-let maxTime = 20;
-let timeLeft = maxTime;
-let flips = 0;
-let matchedCard = 0;
-let disableDeck = false;
-let isPlaying = false;
-let cardOne, cardTwo, timer;
+static int cathodePin[] = {0,0,0,1,1,1,2,2,2,3};
+static int anodePin[] =   {1,2,3,0,2,3,0,1,3,0};
 
-function initTimer() {
-    if(timeLeft <= 0) {
-        return clearInterval(timer);
-    }
-    timeLeft--;
-    timeTag.innerText = timeLeft;
+void setup()
+{
+  turnLightsOff();
 }
 
-function flipCard({target: clickedCard}) {
-    if(!isPlaying) {
-        isPlaying = true;
-        timer = setInterval(initTimer, 1000);
-    }
-    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
-        flips++;
-        flipsTag.innerText = flips;
-        clickedCard.classList.add("flip");
-        if(!cardOne) {
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
-        matchCards(cardOneImg, cardTwoImg);
-    }
+void turnLightsOff() {
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
 }
 
-function matchCards(img1, img2) {
-    if(img1 === img2) {
-        matchedCard++;
-        if(matchedCard == 6 && timeLeft > 0) {
-            return clearInterval(timer);
-        }
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = "";
-        return disableDeck = false;
-    }
+void turnLightOn(int index) {
+  turnLightsOff();
+  
+  digitalWrite(cathodePin[index], LOW);
+  digitalWrite(anodePin[index], HIGH);
+  
+  pinMode(cathodePin[index], OUTPUT);
+  pinMode(anodePin[index], OUTPUT);
+};
 
-    setTimeout(() => {
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400);
+void loop()
+{
+  int i;
+  for (i = 0; i < 9; ++i) { turnLightOn(i); delay(100); }
+  for (i = 9; i >= 0; --i) { turnLightOn(i); delay(100); }
+  for (i = 9; i > 0; --i) { turnLightOn(i); delay(100); }
+  for (i = 0; i < 10; ++i) { turnLightOn(i); delay(100); }
 
-    setTimeout(() => {
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = "";
-        disableDeck = false;
-    }, 1200);
+  for (int i = 0; i < 200; ++i) {
+    turnLightOn(random(10));
+    delay(25);
+  }
 }
-
-function shuffleCard() {
-    timeLeft = maxTime;
-    flips = matchedCard = 0;
-    cardOne = cardTwo = "";
-    clearInterval(timer);
-    timeTag.innerText = timeLeft;
-    flipsTag.innerText = flips;
-    disableDeck = isPlaying = false;
-
-    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-
-    cards.forEach((card, index) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector(".back-view img");
-        setTimeout(() => {
-            imgTag.src = `images/img-${arr[index]}.png`;
-        }, 500);
-        card.addEventListener("click", flipCard);
-    });
-}
-
-shuffleCard();
-
-refreshBtn.addEventListener("click", shuffleCard);
-
-cards.forEach(card => {
-    card.addEventListener("click", flipCard);
-});
